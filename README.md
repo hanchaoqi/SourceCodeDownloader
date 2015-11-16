0b444960f79ebe38e969a8ddf35a7584f9a533ac
hanchaoqi <hanchao_qi@qq.com>
2015-11-16 18:40:35 +0800

    Add firefox in downloader,write README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SourceCodeDownloader
-一个下载器，从一些软件的官方网站下载源代码
+>一个下载器，从一些软件的官方网站下载源代码
+
+###使用方法：
+将软件版本号按行写入以软件名命名的txt文件中，使用命令: python downloader.py yoursoftwareName
+
+###支持的软件：
+- chrome
+    - 下载地址：https://chromium.googlesource.com/chromium/src.git/+refs
+    - 支持所有版本
+- linux_kernel
+    - 下载地址：https://www.kernel.org/pub/linux/kernel/
+    - 支持所有版本
+- firefox
+    - 下载地址： http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/
+    - 支持除以lastest开头的所有版本
--- downloader.py
+++ downloader.py
@@ -8,7 +8,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 
 def communicate(commandLine):
-    print('Executing command:%s...' % commandLine)
+    #print('Executing command:%s...' % commandLine)
     process = Popen(commandLine, stdout=PIPE, stderr=PIPE, shell=True)
     return process.communicate()
 
@@ -22,9 +22,10 @@ def getVersions(versionsFile):
 def init(softwareName):
     '''init needed data,output command list'''
     dictForCommand ={'chrome':'wget -P chrome https://chromium.googlesource.com/chromium/src.git/+archive/%s.tar.gz',
-                'linux_kernel':'wget -P linux_kernel https://www.kernel.org/pub/linux/kernel/v%s/linux-%s.tar.xz'}
+                'linux_kernel':'wget -P linux_kernel https://www.kernel.org/pub/linux/kernel/v%s/linux-%s.tar.xz',
+                'firefox':'wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/%s/source/firefox-%s.source.tar.bz2'}
     
-    versionsFile = softwareName + ".txt"
+    versionsFile = softwareName + "1.txt"
     baseCommand = dictForCommand[softwareName]
     commands = []
     
@@ -36,10 +37,32 @@ def init(softwareName):
     elif softwareName == "linux_kernel":
         for version in open(versionsFile):
             version = version.strip()
-            if version[0] == "4"
+            if version[0] == "4":
                 command = baseCommand % ("4.x",version)
             else:
                 command = baseCommand % (version[:3],version)
+            commands.append(command)
+    elif softwareName == "firefox":
+        for version in open(versionsFile):
+            version = version.strip()
+            baseCommand = dictForCommand[softwareName]
+            if version.split(".")[0] >= "1":
+                if version[:3] == "1.0" or version[:3] == "2.0" or version[:3] == "3.0":
+                    baseCommand = "wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/%s/source/firefox-%s-source.tar.bz2"
+                if version[-4:] == "real":
+                    command = baseCommand % (version,version.split("-")[0])
+                else:
+                    command = baseCommand % (version,version) 
+            elif version[:3] == "0.9":
+                command = "wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/%s/firefox-%s-source.tar.bz2" % (version,version)
+            elif version == "0.10" or version == "0.10.1":
+                command = "wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/0.10/firefox-1.0PR-source.tar.bz2"
+            elif version == "0.10rc":
+                command = "wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/0.10rc/firefox-1.0PRrc-i686-linux-gtk2%2Bxft.tar.gz"
+            elif version == "0.8":
+                command = "wget -P firefox http://ftp.mozilla.org/pub/firefox/releases/0.8/firefox-source-0.8.tar.bz2"
+            else:
+                break;
             commands.append(command)
     else:
         print "Software Name Wrong!"
