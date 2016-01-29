c31725516633a9566d7496de911e2c4e6381a1c4
hanchaoqi <hanchao_qi@qq.com>
2016-01-29 17:47:03 +0800

    add some software

---
+++ Downloader/Downloader/spiders/openjpeg.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from subprocess import Popen,PIPE
+from multiprocessing.dummy import Pool as ThreadPool
+from urlparse import urljoin
+
+def communicate(commandLine):
+    process = Popen(commandLine,stdout=PIPE,stderr=PIPE,shell=True)
+    return process.communicate()
+
+class OpenjpegSpider(scrapy.Spider):
+    name = 'openjpeg'
+    allowed_domains = ['github.com']
+    start_urls = ['https://github.com/openjpeg/openjpeg/releases']
+
+    def parse(self,response):
+        domain = 'https://github.com'
+        sel = Selector(response)
+        urls = sel.xpath('//ul[@class="tag-references"]/li[3]/a/@href').extract()
+        for url in urls:
+            command = "wget -P openjpeg " + urljoin(domain,url)
+            self.log(command)
+            communicate(command)
+        pages = sel.xpath('//div[@class="pagination"]/a[contains(.,"Next")]/@href').extract()
+        for page in pages:
+            yield scrapy.Request(page,callback=self.parse)
